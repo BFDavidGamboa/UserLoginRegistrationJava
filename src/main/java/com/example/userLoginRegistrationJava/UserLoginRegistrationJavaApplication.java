cef3df6aec7988c4a2bb3899f6b1eaa7f53eeb08
BFDavidGamboa <david.gamboa@blankfactor.com>
2023-08-29 12:06:22 -0500

    Added spring initialzr

---
+++ src/main/java/com/example/userLoginRegistrationJava/UserLoginRegistrationJavaApplication.java
@@ -0,0 +1,13 @@
+package main.java.com.example.userLoginRegistrationJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserLoginRegistrationJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserLoginRegistrationJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/userLoginRegistrationJava/UserLoginRegistrationJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package test.java.com.example.userLoginRegistrationJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserLoginRegistrationJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
